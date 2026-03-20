8df2e3548db7c38127c61dc24eedb4dd22ebaf28
vishnu sasikumar <vishnu.sasikumar@pptservices.com>
2026-03-20 09:30:43 +0530

    pos stock restrict price changed for v19

--- pos_negative_stock_restrict/__manifest__.py
+++ pos_negative_stock_restrict/__manifest__.py
@@ -55,7 +55,7 @@ Ideal for businesses that require strict stock control and want to avoid oversel
 
     'images': ['static/description/banner.png'],
     'license': "LGPL-3",
-    'price': 25.0,
+    'price': 10.0,
     'currency': 'USD',
 
     'installable': True,
