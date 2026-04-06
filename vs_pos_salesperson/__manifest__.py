b2985e58b986521ada0ce1a8c58a2b21322ba9b9
vishnu sasikumar <vishnu.sasikumar@pptservices.com>
2026-04-06 14:03:52 +0530

    banner changed2

--- vs_pos_salesperson/__manifest__.py
+++ vs_pos_salesperson/__manifest__.py
@@ -19,7 +19,7 @@
             "vs_pos_salesperson/static/src/xml/PaymentScreenSalesperson.xml",
         ],
     },
-    "images": ['static/description/banner.jpeg'],
+    "images": ['static/description/banner.png'],
     "auto_install": False,
     "installable": True,
     "application": True,
